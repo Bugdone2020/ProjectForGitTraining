d60f66966ece868e8f244b95def77a9a59d27918
bohdankozlov@gmail.com <bohdankozlov@gmail.com>
2022-02-20 19:13:51 +0200

    first comit / some change 1 in Master branch

--- ProjectForGitTraining/Program.cs
+++ ProjectForGitTraining/Program.cs
@@ -7,6 +7,7 @@ namespace ProjectForGitTraining
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("some change 1 in Master branch");
         }
     }
 }
