d0820bf0bee853fe4b1c16edefe8f986b8667f99
bohdankozlov@gmail.com <bohdankozlov@gmail.com>
2022-02-20 19:19:50 +0200

    first commit in BranchToAddHiBohdan

--- ProjectForGitTraining/Program.cs
+++ ProjectForGitTraining/Program.cs
@@ -9,6 +9,8 @@ namespace ProjectForGitTraining
             Console.WriteLine("Hello World!");
             Console.WriteLine("some change 1 in Master branch");
             Console.WriteLine("some change 2 in Master branch");
+            Console.WriteLine("Hi Bohdan and Olya ");
+            Console.WriteLine("Hi Bohdan and Olya ");
         }
     }
 }
